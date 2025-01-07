48aefdfbddfd906a165b14ba46d9561a91591c51
nsatalt <nsatalt@gmail.com>
2025-01-07 13:27:49 +0900

    final?

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from linebot import LineBotApi, WebhookHandler
 from linebot.exceptions import InvalidSignatureError
 from linebot.models import MessageEvent, TextMessage, TextSendMessage
 from dotenv import load_dotenv
+from chat import chat_completion
 
 load_dotenv(override=True)
 
@@ -40,7 +41,8 @@ def callback():
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    line_bot_api.reply_message(event.reply_token, TextSendMessage(text=event.message.text))
+    reply_message = chat_completion(event.message.text)
+    line_bot_api.reply_message(event.reply_token, TextSendMessage(text=reply_message))
 
 
 if __name__ == "__main__":
